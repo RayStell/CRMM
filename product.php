51890afc4b305dd3b05385951cc0e8d5178e9ee1
Your Name <you@example.com>
2025-02-14 10:59:13 +0700

    qr-code

--- product.php
+++ product.php
@@ -12,6 +12,15 @@ AuthCheck('', 'login.php');
 
 require_once 'api/helpers/InputDefaultValue.php';
 
+require 'vendor/autoload.php';
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
+$qrCode = new QrCode('ИДИ НА ХУЙ');
+$writer = new PngWriter();
+$result = $writer->write($qrCode);
+header('Content-Type: '.$result->getMimeType());
+echo $result->getString();
+
 /**
  * 1.фильрация / сортировка
  * 2.вывод продуктов
